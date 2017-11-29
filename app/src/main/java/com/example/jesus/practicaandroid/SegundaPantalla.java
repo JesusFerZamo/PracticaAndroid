745e4e7944be50966d7f21e565ae9941017a78b7
luisaoprog <luiszamora1995@gmail.com>
2017-11-29 12:48:09 +0100

    ListView fixed.

--- app/src/main/java/com/example/jesus/practicaandroid/SegundaPantalla.java
+++ app/src/main/java/com/example/jesus/practicaandroid/SegundaPantalla.java
@@ -4,14 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
 public class SegundaPantalla extends AppCompatActivity {
 
-    Button btnAcep;
+    //Button btnAcep;
     ListView lst;
     TextView txtPrinci;
 
@@ -25,7 +25,7 @@ public class SegundaPantalla extends AppCompatActivity {
         final String[] datos = new String[]{"Acceso a Datos","Android Studio","Desarrollo de Interfaces"};
         ArrayAdapter<String> adaptador = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_checked, datos);
 
-        btnAcep = (Button) findViewById(R.id.btnAcep);
+        //btnAcep = (Button) findViewById(R.id.btnAcep);
         lst = (ListView) findViewById(R.id.lstOpc);
         txtPrinci = (TextView) findViewById(R.id.txtPrinci);
 
@@ -33,9 +33,29 @@ public class SegundaPantalla extends AppCompatActivity {
         txtPrinci.setText(nombre);
 
         lst.setAdapter(adaptador);
+
+
+        lst.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                        if (position == 0) {
+                            // Abre una nueva Activity:
+                            Intent myIntent = new Intent(view.getContext(), PrimeraPantalla.class);
+                            startActivity(myIntent);
+                        } else if (position == 1) {
+                            // Abre una nueva Activity:
+                            Intent myIntent = new Intent(view.getContext(), TerceraPantalla.class);
+                        }
+                    }
+
+
+                } );
+
     }
-    public void pulsaBoton(View v){
-        Intent intencion = new Intent(getApplicationContext(),TerceraPantalla.class);
-        startActivity(intencion);
-    }
+
+
 }
+
+
